09c9eeec6063f8877c11ca37d27528388f307a7d
Gentlemen Samurai <adam.kordykiewicz@gmail.com>
2021-03-09 21:01:46 +0100

    Creating window

--- open_mate_karate/main.cpp
+++ open_mate_karate/main.cpp
@@ -1,6 +1,51 @@
 #include "libs.h"
 
+void framebufferResizeCallback(GLFWwindow* window, int fbW, int fbH)
+{
+    glViewport(0, 0, fbW, fbH);
+}
+
 int main()
 {
+    glfwInit();
+
+    const int WINDOW_WIDTH = 640;
+    const int WINDOW_HEIGHT = 480;
+    int framebufferWidth = 0;
+    int framebufferHeight = 0;
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Open Mate Karate", nullptr, nullptr);
+
+    glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
+    glfwGetFramebufferSize(window, &framebufferWidth, &framebufferWidth);
+    glViewport(0, 0, framebufferWidth, framebufferHeight);
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+
+    if(glewInit() != GLEW_OK)
+    {
+        std::cout << "Error: Glew init failed!" << std::endl;
+        glfwTerminate();
+    }
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glFlush();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
