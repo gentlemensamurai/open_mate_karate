9ae6d12a679c320568d0294f268b71adf62b0f06
Gentlemen Samurai <adam.kordykiewicz@gmail.com>
2021-03-14 18:59:52 +0100

    Separate color and position buffers

--- open_mate_karate/main.cpp
+++ open_mate_karate/main.cpp
@@ -43,19 +43,30 @@ int main()
         return -1;
     }
 
-    GLfloat vertices[]
-    {
-         // POSITION        // COLOR
-         0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+    GLfloat vertPos[]
+    { 
+         0.0f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
     };
 
-    GLuint vbo;
+    GLfloat vertColor[]
+    {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    };
 
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    GLuint vboPos;
+    GLuint vboColor;
+
+    glGenBuffers(1, &vboPos);
+    glBindBuffer(GL_ARRAY_BUFFER, vboPos);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertPos), vertPos, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &vboColor);
+    glBindBuffer(GL_ARRAY_BUFFER, vboColor);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertColor), vertColor, GL_STATIC_DRAW);
 
     GLuint vao;
 
@@ -63,11 +74,13 @@ int main()
     glBindVertexArray(vao);
 
     // POSITION
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, nullptr);
+    glBindBuffer(GL_ARRAY_BUFFER, vboPos);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
     glEnableVertexAttribArray(0);
 
     // COLOR
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, (GLvoid*)(sizeof(GLfloat) * 3));
+    glBindBuffer(GL_ARRAY_BUFFER, vboColor);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
     glEnableVertexAttribArray(1);
 
     // VERTEX SHADER
@@ -146,7 +159,7 @@ int main()
 
     glDeleteProgram(shaderProgram);
     glDeleteVertexArrays(1, &vao);
-    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &vboPos);
     glfwTerminate();
     return 0;
 }
