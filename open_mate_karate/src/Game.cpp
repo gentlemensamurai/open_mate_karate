98aceb67c9d78efc4b9ddf7a9291de60d5c8e1ba
Gentlemen Samurai <adam.kordykiewicz@gmail.com>
2021-03-27 18:51:03 +0100

    Refactor

--- open_mate_karate/src/Game.cpp
+++ open_mate_karate/src/Game.cpp
@@ -8,6 +8,14 @@ void Game::setFramebufferSizeCallback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+void Game::setScrollCallback(GLFWwindow* window, double deltaX, double deltaY)
+{
+    //double fieldOfView { camera.getFieldOfView() + deltaY * ZOOM_SENSITIVITY };
+    //fieldOfView = glm::clamp(fieldOfView, 1.0, 120.0);
+
+    //camera.setFieldOfView((float)fieldOfView);
+}
+
 Game::Game
 (
     const std::string& title,
@@ -25,9 +33,23 @@ Game::Game
       VERSION_MINOR(versionMinor),
       FULLSCREEN(fullscreen),
       RESIZABLE(resizable),
+      ZOOM_SENSITIVITY(-3.0),
+      CAMERA_MOVE_SPEED(5.0f),
+      MOUSE_SENSITIVITY(0.1f),
       window(nullptr),
       framebufferWidth(WINDOW_WIDTH),
-      framebufferHeight(WINDOW_HEIGHT)
+      framebufferHeight(WINDOW_HEIGHT),
+      wireframe(false),
+      flashlightOn(true),
+      model(1.0f),
+      view(1.0f),
+      projection(1.0f),
+      camera(glm::vec3(0.0f, 3.0f, 10.0f)),
+      modelsPositions(0),
+      modelsScales(0),
+      meshes(0),
+      textures(0),
+      basicShader()
 {
     initGlfw();
     initWindow();
@@ -41,24 +63,93 @@ Game::~Game()
     glfwTerminate();
 }
 
-void Game::update()
+void Game::run()
 {
+    // MODEL POSITION
+    modelsPositions.push_back(glm::vec3(-3.5f, 0.0f, 0.0f)); // CRATE 1
+    modelsPositions.push_back(glm::vec3(3.5f, 0.0f, 0.0f)); // CRATE 2
+    modelsPositions.push_back(glm::vec3(0.0f, 0.0f, -2.0f)); // ROBOT
+    modelsPositions.push_back(glm::vec3(0.0f, 0.0f, 0.0f)); // FLOOR
+    modelsPositions.push_back(glm::vec3(0.0f, 0.0f, 2.0f)); // PIN
+    modelsPositions.push_back(glm::vec3(-2.0f, 0.0f, 2.0f)); // BUNNY
+    modelsPositions.push_back(glm::vec3(-5.0f, 0.0f, 0.0f)); // LAMP POST
 
+    // MODEL SCALE
+    modelsScales.push_back(glm::vec3(1.0f, 1.0f, 1.0f)); // CRATE 1
+    modelsScales.push_back(glm::vec3(1.0f, 1.0f, 1.0f)); // CRATE 2
+    modelsScales.push_back(glm::vec3(1.0f, 1.0f, 1.0f)); // ROBOT
+    modelsScales.push_back(glm::vec3(10.0f, 1.0f, 10.0f)); // FLOOR
+    modelsScales.push_back(glm::vec3(0.1f, 0.1f, 0.1f)); // PIN
+    modelsScales.push_back(glm::vec3(0.7f, 0.7f, 0.7f)); // BUNNY
+    modelsScales.push_back(glm::vec3(1.0f, 1.0f, 1.0f)); // LAMP POST
+
+    // SHADER PROGRAM
+    basicShader.loadShaders("shaders/basic_spot.vert", "shaders/basic_spot.frag");
+
+    // LOAD MESHES AND TEXTURES
+    meshes.resize(modelsPositions.size());
+    textures.resize(modelsPositions.size());
+
+    meshes[0].loadObj("models/crate.obj");
+    meshes[1].loadObj("models/woodcrate.obj");
+    meshes[2].loadObj("models/robot.obj");
+    meshes[3].loadObj("models/floor.obj");
+    meshes[4].loadObj("models/bowling_pin.obj");
+    meshes[5].loadObj("models/bunny.obj");
+    meshes[6].loadObj("models/lampPost.obj");
+
+    textures[0].loadTexture("textures/crate_diffuse.jpg", true);
+    textures[1].loadTexture("textures/woodcrate_diffuse.jpg", true);
+    textures[2].loadTexture("textures/robot_diffuse.jpg", true);
+    textures[3].loadTexture("textures/tile_floor.jpg", true);
+    textures[4].loadTexture("textures/AMF.jpg", true);
+    textures[5].loadTexture("textures/bunny_diffuse.jpg", true);
+    textures[6].loadTexture("textures/lamp_post_diffuse.png", true);
+
+    glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+    glViewport(0, 0, framebufferWidth, framebufferHeight);
+
+    double lastTime { glfwGetTime() };
+
+    while (!glfwWindowShouldClose(window))
+    {
+        showFps();
+
+        double currentTime { glfwGetTime() };
+        double deltaTime { currentTime - lastTime };
+
+        glfwPollEvents();
+
+        update(deltaTime);
+        render();
+
+        lastTime = currentTime;
+    }
 }
 
-void Game::render()
+void Game::showFps()
 {
+    static double previousSeconds { 0.0 };
+    static int frameCount { 0 };
 
-}
+    double currentSeconds { glfwGetTime() };
+    double elapsedSeconds { currentSeconds - previousSeconds };
 
-int Game::getWindowShouldClose()
-{
-    return glfwWindowShouldClose(window);
-}
+    if (elapsedSeconds > 0.25)
+    {
+        previousSeconds = currentSeconds;
+        double fps { static_cast<double>(frameCount) / elapsedSeconds };
+        double msPerFrame { 1000.0 / fps };
 
-void Game::setWindowShouldClose()
-{
-    glfwSetWindowShouldClose(window, GLFW_TRUE);
+        std::ostringstream outs;
+        outs.precision(3);
+        outs << std::fixed << WINDOW_TITLE << " (FPS: " << fps << ", frame time: " << msPerFrame << " ms)";
+        glfwSetWindowTitle(window, outs.str().c_str());
+
+        frameCount = 0;
+    }
+
+    frameCount++;
 }
 
 void Game::initGlfw()
@@ -76,7 +167,6 @@ void Game::initWindow()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, VERSION_MAJOR);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, VERSION_MINOR);
     glfwWindowHint(GLFW_RESIZABLE, RESIZABLE);
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE); CHECK IT
 
     if (!FULLSCREEN)
     {
@@ -102,9 +192,14 @@ void Game::initWindow()
     }
 
     glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
-    glfwSetFramebufferSizeCallback(window, setFramebufferSizeCallback);
 
     glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, setFramebufferSizeCallback);
+    glfwSetScrollCallback(window, setScrollCallback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPos(window, framebufferWidth / 2.0f, framebufferHeight / 2.0);
 }
 
 void Game::initGlew()
@@ -130,8 +225,120 @@ void Game::initOpenGlOptions()
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
 
-    //glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
-    //glViewport(0, 0, windowWidth, windowHeight);
-    
+void Game::update(double deltaTime)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+    {
+        wireframe = !wireframe;
+
+        if (wireframe)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+        else
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+    {
+        flashlightOn = !flashlightOn;
+    }
+
+    double mouseX { 0.0 };
+    double mouseY { 0.0 };
+
+    glfwGetCursorPos(window, &mouseX, &mouseY);
+
+    float yaw { static_cast<float>(framebufferWidth / 2 - mouseX) * MOUSE_SENSITIVITY };
+    float pitch { static_cast<float>(framebufferHeight / 2 - mouseY) * MOUSE_SENSITIVITY };
+
+    camera.rotate(yaw, pitch);
+
+    glfwSetCursorPos(window, framebufferWidth / 2.0, framebufferHeight / 2.0);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        camera.move(CAMERA_MOVE_SPEED * (float)deltaTime * camera.getLook());
+    }
+    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        camera.move(CAMERA_MOVE_SPEED * (float)deltaTime * -camera.getLook());
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        camera.move(CAMERA_MOVE_SPEED * (float)deltaTime * -camera.getRight());
+    }
+    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        camera.move(CAMERA_MOVE_SPEED * (float)deltaTime * camera.getRight());
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+    {
+        camera.move(CAMERA_MOVE_SPEED * (float)deltaTime * -camera.getUp());
+    }
+    else if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+    {
+        camera.move(CAMERA_MOVE_SPEED * (float)deltaTime * camera.getUp());
+    }
+}
+
+void Game::render()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    view = camera.getViewMatrix();
+    projection = glm::perspective(glm::radians(camera.getFieldOfView()), static_cast<float>(framebufferWidth) / static_cast<float>(framebufferHeight), 0.1f, 100.0f);
+
+    glm::vec3 viewPos;
+    viewPos.x = camera.getPos().x;
+    viewPos.y = camera.getPos().y;
+    viewPos.z = camera.getPos().z;
+
+    glm::vec3 lightPos = camera.getPos();
+    glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
+    lightPos.y -= 0.5f;
+
+    basicShader.use();
+
+    basicShader.setUniform("view", view);
+    basicShader.setUniform("projection", projection);
+    basicShader.setUniform("viewPos", viewPos);
+
+    basicShader.setUniform("light.ambient", glm::vec3(0.2f, 0.2f, 0.2f));
+    basicShader.setUniform("light.diffuse", lightColor);
+    basicShader.setUniform("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+    basicShader.setUniform("light.position", lightPos);
+    basicShader.setUniform("light.direction", camera.getLook());
+    basicShader.setUniform("light.constant", 1.0f);
+    basicShader.setUniform("light.linear", 0.07f);
+    basicShader.setUniform("light.exponent", 0.017f);
+    basicShader.setUniform("light.cosInnerCone", glm::cos(glm::radians(15.0f)));
+    basicShader.setUniform("light.cosOuterCone", glm::cos(glm::radians(20.0f)));
+    basicShader.setUniform("light.on", flashlightOn);
+
+    for (size_t i { 0 }; i < modelsPositions.size(); i++)
+    {
+        model = glm::translate(glm::mat4(1.0f), modelsPositions[i]) * glm::scale(glm::mat4(1.0f), modelsScales[i]);
+        basicShader.setUniform("model", model);
+        basicShader.setUniform("material.ambient", glm::vec3(0.1f, 0.1f, 0.1f));
+        basicShader.setUniformSampler("material.diffuseMap", 0);
+        basicShader.setUniform("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));
+        basicShader.setUniform("material.shininess", 150.0f);
+        textures[i].bind(0);
+        meshes[i].draw();
+        textures[i].unbind(0);
+    }
+
+    glfwSwapBuffers(window);
 }
--- open_mate_karate/src/Game.h
+++ open_mate_karate/src/Game.h
@@ -2,11 +2,16 @@
 #define GAME_H
 
 #include "libs.h"
+#include "Camera.h"
+#include "ShaderProgram.h"
+#include "Mesh.h"
+#include "Texture2D.h"
 
 class Game
 {
 public:
     static void setFramebufferSizeCallback(GLFWwindow* window, int width, int height);
+    static void setScrollCallback(GLFWwindow* window, double deltaX, double deltaY);
 
     Game
     (
@@ -20,10 +25,9 @@ public:
     );
     virtual ~Game();
 
-    void update();
-    void render();
-    int getWindowShouldClose();
-    void setWindowShouldClose();
+    void run();
+
+    void showFps();
 
 private:
     const std::string WINDOW_TITLE;
@@ -33,15 +37,36 @@ private:
     const int VERSION_MINOR;
     const bool FULLSCREEN;
     const bool RESIZABLE;
+    const double ZOOM_SENSITIVITY;
+    const float CAMERA_MOVE_SPEED;
+    const float MOUSE_SENSITIVITY;
 
     GLFWwindow* window;
     int framebufferWidth;
     int framebufferHeight;
+    bool wireframe;
+    bool flashlightOn;
+
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 projection;
+
+    Camera camera;
+
+    std::vector<glm::vec3> modelsPositions;
+    std::vector<glm::vec3> modelsScales;
+    std::vector<Mesh> meshes;
+    std::vector<Texture2D> textures;
+
+    ShaderProgram basicShader;
 
     void initGlfw();
     void initWindow();
     void initGlew();
     void initOpenGlOptions();
+
+    void update(double deltaTime);
+    void render();
 };
 
 #endif // GAME_H
