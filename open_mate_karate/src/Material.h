7e4b46d845211ad2e1ed0aa3d89ee1a1f51e55d2
Gentlemen Samurai <adam.kordykiewicz@gmail.com>
2021-03-28 11:04:59 +0200

    Model and Material classes

---
+++ open_mate_karate/src/Material.h
@@ -0,0 +1,14 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+class Material
+{
+public:
+    Material();
+    ~Material();
+
+private:
+
+};
+
+#endif MATERIAL_H
---
+++ open_mate_karate/src/Model.h
@@ -0,0 +1,18 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "ShaderProgram.h"
+#include "Mesh.h"
+#include "Texture2D.h"
+
+class Model
+{
+public:
+    Model();
+    ~Model();
+
+private:
+
+};
+
+#endif // MODEL_H
